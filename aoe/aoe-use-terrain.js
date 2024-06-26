01e64c199e8de9c4b67debffac7a18e1f66d10da
pcmath <163906776+pcmath@users.noreply.github.com>
2024-06-26 11:34:09 -0400

    Add files via upload
    Added instructions, the possibility to refer directly to terrain change and TransformData object to hold different terrain transform data.

--- aoe/aoe-use-terrain.js
+++ aoe/aoe-use-terrain.js
@@ -1,3 +1,51 @@
+/*
+Transform terrain based on item.custom.aoe.terrainTransform
+terrainTransform should be either null, a string or transformData
+If terrainTransform is a string, the transformData will be taken from terrain.custom.transform[key],
+Where key is the string.
+Examples of transformData are in the TransformData object below.
+I suggest using referring to this object for your trasnformData.
+Example for terrain transform data in terrain custom parameters to change to forest for terrainTransform "grow":
+{
+	transform: {
+		"grow": TransformData.RTPforest
+	}
+}
+Example for item to match this:
+{
+	aoe: {
+		//usual AoE stuff here
+		terrainTransform:"grow"
+	}
+}
+Example for item using direct transform data:
+{
+	aoe: {
+		//usualAoe stuff here
+		terrainTransform: TransformData.RTPforest
+	}
+}
+You can block a terrain's transformation using terrain.custom.noTransform {noTransform:true}
+*/
+
+TransformData = {
+	RTPplain: {
+		xSrc: 0,	//x position in sheet
+		ySrc: 0,	//y position in sheet
+		id: 0,		//sheet id
+		rtp: true,	//if the sheet is RTP
+		transparent: false
+	},
+
+	RTPforest: {
+		xSrc:0,
+		ySrc:5,
+		id:0,
+		rtp:true,
+		transparent: false
+	}
+}
+
 var AoeItemTerrainUse = defineObject(BaseItemUse,
 {
 	_dynamicEvent: null,
@@ -33,17 +81,42 @@ var AoeItemTerrainUse = defineObject(BaseItemUse,
 			x = CurrentMap.getX(indexArray[i]);
 			y = CurrentMap.getY(indexArray[i]);
 			terrain = PosChecker.getTerrainFromPos(x, y);
-			if(terrain && terrain.custom.transform && terrain.custom.transform[this._transformType]) {
-				transformData = terrain.custom.transform[this._transformType];
-				handle = root.createResourceHandle(transformData.rtp, transformData.id, 0, transformData.xSrc, transformData.ySrc);
-				root.getCurrentSession().setMapChipGraphicsHandle(x, y, transformData.transparent, handle)
+			transformData = this._getTransformData(terrain, this._transformType);
+			if(!transformData) {
+				continue ;
 			}
+			handle = root.createResourceHandle(transformData.rtp, transformData.id, 0, transformData.xSrc, transformData.ySrc);
+			root.getCurrentSession().setMapChipGraphicsHandle(x, y, transformData.transparent, handle)
 		}
-		return  MoveResult.END;
+	},
+
+	_getTransformData: function(terrain, transformType) {
+		if(!terrain || terrain.custom.noTransform) {
+			return null;
+		}
+		if(typeof(transformType) == "string") {
+			return this._getDynamicTransformData(terrain, transformType);
+		}
+		return this._getStaticTransformData(terrain, transformType);
+	},
+
+	_getDynamicTransformData: function(terrain, transformType) {
+		if(terrain.custom.transform && terrain.custom.transform[this._transformType]) {
+			return terrain.custom.transform[this._transformType];
+		}
+		return null;
+	},
+
+	_getStaticTransformData: function(terrain, transformType) {
+		return transformType;
+	},
+
+	_moveMainUseCycle: function() {
+
 	},
 
 	drawMainUseCycle: function() {
-		var mode = this.getCycleMode();
-		var result = MoveResult.CONTINUE;
+		//var mode = this.getCycleMode();
+		//var result = MoveResult.CONTINUE;
 	}
 });
